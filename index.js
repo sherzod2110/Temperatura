07338df49156e407dcc0ee5ed1ef75e29e94baed
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-04-21 20:40:23 +0500

    gfnfgfg

---
+++ index.js
@@ -0,0 +1,18 @@
+let elForm = document.querySelector('.form');
+let elCelsiusInput = document.querySelector('.celsius-input');
+let elFahrenheitinput = document.querySelector('.fahrenheit-input');
+
+let elCelciusResult = document.querySelector('.result-celsius');
+
+ elCelsiusInput.addEventListener('input', function(){
+  let elFahrenheitResult = parseFloat(elCelsiusInput.value.trim(), 10) * 9/5 + 32;
+  elFahrenheitinput.value = elFahrenheitResult;
+  elCelciusResult.textContent = elCelsiusInput.value + '°C×9/5+32 =' + elFahrenheitResult;
+ });
+
+
+ elFahrenheitinput.addEventListener('input', function(){
+  let elCelsiusResult = (parseFloat(elFahrenheitinput.value.trim(), 10)- 32 ) * 5/9;
+  elCelsiusInput.value = elCelsiusResult;
+  elCelciusResult.textContent = '(' + elFahrenheitinput.value + '°F-32)×5/9 =' + elCelsiusResult;
+ })
